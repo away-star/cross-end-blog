9415d5f88b4a6f43b5f29824333394efa7ead685
xing xing <2064989403@qq.com>
2023-04-22 16:25:25 +0800

    Update proxy.ts
    update the proxy

--- config/proxy.ts
+++ config/proxy.ts
@@ -11,7 +11,7 @@ export default {
     // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
     '/api/': {
       // 要代理的地址
-      target: 'http://localhost:9000',
+      target: 'https://6cfa-39-129-5-52.ngrok-free.app',
       // 配置了这个可以从 http 代理到 https
       // 依赖 origin 的功能可能需要这个，比如 cookie
       changeOrigin: true,
