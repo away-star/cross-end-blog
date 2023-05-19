9274bf8bebc20c83bda43e2175054492e583cbf5
xing xing <2064989403@qq.com>
2023-05-19 14:17:17 +0800

    Update proxy.ts

--- api/proxy.ts
+++ api/proxy.ts
@@ -3,6 +3,8 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = ({req, res}: { req: any, res: any }) => {
+
+
     let target = ''
     // 代理目标地址
     // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
